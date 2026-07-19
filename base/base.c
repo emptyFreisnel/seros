403b0f9f3cf6f8410a45aa9ff14a3c3cd9be1537
emptyFreisnel <freisnelprincess@gmail.com>
2026-07-19 23:56:43 +0800

    seros: add new files
     Changes to be committed:
    	new file:   base/base.c
    	new file:   base/base_list.c
    	new file:   base/base_memory.c

---
+++ base/base.c
@@ -0,0 +1,9 @@
+#include "headers/base_core.h"
+#include "headers/base_list.h"
+#include "headers/base_memory.h"
+#include "headers/base_string.h"
+
+#include "base_core.c"
+#include "base_list.c"
+#include "base_memory.c"
+

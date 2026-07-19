018531ab91748c9f180ffec7229a71075bca275f
emptyFreisnel <freisnelprincess@gmail.com>
2026-07-19 23:34:28 +0800

    * base/base_core.c: changed int prefix from S32/S64 to I32/I64
     Changes to be committed:
    	modified:   base/base_core.c
    
    	Reason: To reserve the S prefix for string types instead.

--- base/base_core.c
+++ base/base_core.c
@@ -1,50 +1,38 @@
-/*..........................................................................*/
-//* Includes
+/**..........................................................................
+//  Min, Max, Clamp and Swap Functions
 
-#include "base_core.h"
+*  Assume additional stack traffic when using these in debug builds.
+*  Your average optimizing compiler can inline this during release.
+*  See SEI Cert C Coding Standard (Edition 2016): page 26 and 27.           */
 
-/*..........................................................................*/
-//* Min, Max, Clamp and Swap Functions
-
-// C compliers will not substitute and inline function calls in debug builds.
-// There is still additional stack traffic unless it is compiled with optflags.
-// This is just to prevent side effects from using unsafe macros, like the
-// ubiquitous min and max macros we all know (and love?).
-
-// See SEI Cert C Coding Standard (Edition 2016): page 26 and 27.
-
-static inline S16 maxS16(S16 x, S16 y) { return (x > y) ? x : y; };
-static inline S32 maxS32(S32 x, S32 y) { return (x > y) ? x : y; };
-static inline S64 maxS64(S64 x, S64 y) { return (x > y) ? x : y; };
+static inline I16 maxI16(I16 x, I16 y) { return (x > y) ? x : y; };
+static inline I32 maxI32(I32 x, I32 y) { return (x > y) ? x : y; };
+static inline I64 maxI64(I64 x, I64 y) { return (x > y) ? x : y; };
 static inline U16 maxU16(U16 x, U16 y) { return (x > y) ? x : y; };
 static inline U32 maxU32(U32 x, U32 y) { return (x > y) ? x : y; };
 static inline U64 maxU64(U64 x, U64 y) { return (x > y) ? x : y; };
 static inline F32 maxF32(F32 x, F32 y) { return (x > y) ? x : y; };
 static inline F64 maxF64(F64 x, F64 y) { return (x > y) ? x : y; };
                                                               
-static inline S16 minS16(S16 x, S16 y) { return (x < y) ? x : y; };
-static inline S32 minS32(S32 x, S32 y) { return (x < y) ? x : y; };
-static inline S64 minS64(S64 x, S64 y) { return (x < y) ? x : y; };
+static inline I16 minI16(I16 x, I16 y) { return (x < y) ? x : y; };
+static inline I32 minI32(I32 x, I32 y) { return (x < y) ? x : y; };
+static inline I64 minI64(I64 x, I64 y) { return (x < y) ? x : y; };
 static inline U16 minU16(U16 x, U16 y) { return (x < y) ? x : y; };
 static inline U32 minU32(U32 x, U32 y) { return (x < y) ? x : y; };
 static inline U64 minU64(U64 x, U64 y) { return (x < y) ? x : y; };
 static inline F64 minF32(F64 x, F64 y) { return (x < y) ? x : y; };
 static inline F32 minF64(F32 x, F32 y) { return (x < y) ? x : y; };
 
-static inline S32
-clampS32(S32 val, S32 min, S32 max) {
+static inline I32
+clampI32(I32 val, I32 min, I32 max) {
         if (val < min) return min;
         if (val > max) return max;
         return val;
 }
 
 static inline U0
-swapS32(S32 *x, S32 *y) {
-        S32 t = *x;
+swapI32(I32 *x, I32 *y) {
+        I32 t = *x;
         *x = *y;
         *y = t;
 }
-
-/*..........................................................................*/
-
-

86059e33cc1ebf5893b362cd166c72a39fc4e368
emptyFreisnel <freisnelprincess@gmail.com>
2026-07-25 00:03:15 +0800

    * base/base_list.c: X-macro generation for Min, Max, Clamp and Swap functions
     On branch main
     Changes to be committed:
    	modified:   base/base_core.c

--- base/base_core.c
+++ base/base_core.c
@@ -1,38 +1,30 @@
 /**..........................................................................
 //  Min, Max, Clamp and Swap Functions
 
+*  The Max, Min, Clamp and Swap functions are generated using X-macros.
 *  Assume additional stack traffic when using these in debug builds.
 *  Your average optimizing compiler can inline this during release.
-*  See SEI Cert C Coding Standard (Edition 2016): page 26 and 27.           */
+*  See SEI Cert C Coding Standard (Edition 2016): page 26 and 27.          */
 
-static inline I16 maxI16(I16 x, I16 y) { return (x > y) ? x : y; };
-static inline I32 maxI32(I32 x, I32 y) { return (x > y) ? x : y; };
-static inline I64 maxI64(I64 x, I64 y) { return (x > y) ? x : y; };
-static inline U16 maxU16(U16 x, U16 y) { return (x > y) ? x : y; };
-static inline U32 maxU32(U32 x, U32 y) { return (x > y) ? x : y; };
-static inline U64 maxU64(U64 x, U64 y) { return (x > y) ? x : y; };
-static inline F32 maxF32(F32 x, F32 y) { return (x > y) ? x : y; };
-static inline F64 maxF64(F64 x, F64 y) { return (x > y) ? x : y; };
-                                                              
-static inline I16 minI16(I16 x, I16 y) { return (x < y) ? x : y; };
-static inline I32 minI32(I32 x, I32 y) { return (x < y) ? x : y; };
-static inline I64 minI64(I64 x, I64 y) { return (x < y) ? x : y; };
-static inline U16 minU16(U16 x, U16 y) { return (x < y) ? x : y; };
-static inline U32 minU32(U32 x, U32 y) { return (x < y) ? x : y; };
-static inline U64 minU64(U64 x, U64 y) { return (x < y) ? x : y; };
-static inline F64 minF32(F64 x, F64 y) { return (x < y) ? x : y; };
-static inline F32 minF64(F32 x, F32 y) { return (x < y) ? x : y; };
+#define NumTypes(X) X(I16) X(I32) X(I64) X(U16) X(U32) X(U64) X(F32) X(F64)
+#define MakeMaxFn(T) static inline T max##T(T x, T y) { return (x > y) ? x : y; }
+#define MakeMinFn(T) static inline T min##T(T x, T y) { return (x < y) ? x : y; }
+#define MakeSwapFn(T) static inline U0 swap##T(T *x, T*y) { T t = *x; *x = *y; *y = t; }
 
-static inline I32
-clampI32(I32 val, I32 min, I32 max) {
-        if (val < min) return min;
-        if (val > max) return max;
-        return val;
-}
+#define MakeClampFn(T)                          \
+        static inline T                         \
+        clamp##T(T val, T min, T max) {         \
+                if (val < min) return min;      \
+                if (val > max) return max;      \
+                return val;                     \
+        }
 
-static inline U0
-swapI32(I32 *x, I32 *y) {
-        I32 t = *x;
-        *x = *y;
-        *y = t;
-}
+NumTypes(MakeMaxFn)
+NumTypes(MakeMinFn)
+NumTypes(MakeSwapFn)
+NumTypes(MakeClampFn)
+
+#undef MakeMaxFn
+#undef MakeMinFn
+#undef MakeSwapFn
+#undef MakeClampFn

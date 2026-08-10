25899694633d765339c097b998bf8bdb69c4808f
emptyFreisnel <freisnelprincess@gmail.com>
2026-08-10 17:39:02 +0800

    * base/base.c: reordering of includes
     On branch main
     Changes to be committed:
    	modified:   base/base.c

--- base/base.c
+++ base/base.c
@@ -1,9 +1,8 @@
 #include "headers/base_core.h"
+#include "headers/base_scall_lyr.h"
 #include "headers/base_dbgassert.h"
-#include "headers/base_list.h"
+#include "headers/base_utils.h"
 #include "headers/base_memory.h"
 #include "headers/base_string.h"
-
-#include "base_core.c"
-#include "base_list.c"
-#include "base_memory.c"
+#include "headers/base_io.h"
+#include "headers/base_list.h"

bfe0d8cae1a2a193f0104b9778f8884d0ada3a5a
emptyFreisnel <freisnelprincess@gmail.com>
2026-08-27 21:08:51 +0800

    * base/headers/base_dbgassert.h: fixed hintassert macro cascade
     On branch main
     Changes to be committed:
    	modified:   base/headers/base_dbgassert.h

--- base/headers/base_dbgassert.h
+++ base/headers/base_dbgassert.h
@@ -1,27 +1,9 @@
 /**........................................................
 // Asserts.                                              */
 
-#if !defined(COMPILE_ASSERT)
-#       define COMPILE_ASSERT 0
-#endif         /* !COMPILE_ASSERT */
-#if !defined(RUNTIME_ASSERT)
-#       define RUNTIME_ASSERT 0
-#endif         /* !RUNTIME_ASSERT */
-#if !defined(HINT_ASSERT)
-#       define HINT_ASSERT    0
-#endif         /* !HINT_ASSERT */
-
 #if !defined(ASSERT_ALL)
 #       define ASSERT_ALL     1
 #endif         /* !ASSERT_ALL */
-
-#if !defined(ASSERT_USE_HINT)
-#       define ASSERT_USE_HINT 1
-#endif         /* !ASSERT_USE_HINT */
-
-#if !defined(ASSERT_USE_DIAGNOSTIC)
-#       define ASSERT_USE_DIAGNOSTIC 0
-#endif         /* !ASSERT_USE_DIAGNOSTIC */
 
 #if ASSERT_ALL
 #  if !defined(COMPILE_ASSERT)
@@ -35,63 +17,78 @@
 #  endif       /* !HINT_ASSERT    */
 #endif         /*  ASSERT_ALL  */
 
+#if !defined(COMPILE_ASSERT)
+#       define COMPILE_ASSERT 0
+#endif         /* !COMPILE_ASSERT */
+#if !defined(RUNTIME_ASSERT)
+#       define RUNTIME_ASSERT 0
+#endif         /* !RUNTIME_ASSERT */
+#if !defined(HINT_ASSERT)
+#       define HINT_ASSERT    0
+#endif         /* !HINT_ASSERT */
+
+#if !defined(ASSERT_USE_HINT)
+#       define ASSERT_USE_HINT 1
+#endif         /* !ASSERT_USE_HINT */
+
+#if !defined(ASSERT_USE_DIAGNOSTIC)
+#       define ASSERT_USE_DIAGNOSTIC 0
+#endif         /* !ASSERT_USE_DIAGNOSTIC */
+
 #if ASSERT_USE_DIAGNOSTIC
-#       define Diagnostic(s, w) assert_log(s, w);
-#else
-#       define Diagnostic(s, w) ((U0)(s), (U0)(w))
+#       define Diagnostic(s, ...) ((u0)(s), ##__VA_ARGS__)
+#else          /* !ASSERT_USE_DIAGNOSTIC */
+#       define Diagnostic(s, w) ((u0)(s), (u0)(w))
 #endif         /* Diagnostic */
 
 #if COMPILE_ASSERT
 #  if STD_C11 || GNU_COMPILER || CLANG_COMPILER
 #       define CompileAssert(...) _Static_assert(__VA_ARGS__)
 #  else        /* !(STD_C11 || GNU_COMPILER || CLANG_COMPILER) */
-#       define CompileAssert(...) ((U0)0)
+#       define CompileAssert(...) ((u0)0)
 #  endif       /* STD_C11 || GNU_COMPILER || CLANG_COMPILER    */
 #else          /* !COMPILE_ASSERT */
-#       define CompileAssert(...) ((U0)0)
+#       define CompileAssert(...) ((u0)0)
 #endif         /* CompileAssert   */
 
-/* RuntimeAssert: Diagnostic prints are printed out   */
-
-/* static inline U0 */
-/* assert_log(S8 str, U0 *ptr) {}; */
+/* RuntimeAssert: Diagnostic prints are printed out */
 
 #if RUNTIME_ASSERT
-#  if GNU_COMPILER || CLANG_COMPILER
+#  if COMPILER_HAS_BUILTINS
 #       define RuntimeAssert(c) do { if (Unlikely(!(c))) __builtin_trap(); } while(0)
-#  elif MSVC_COMPILER
+#  elif HAS_MSVC_INTRINSICS
 #       define RuntimeAssert(c) do { if (Unlikely(!(c))) __debugbreak();   } while(0)
-#  else        /* !(GNU_COMPILER || CLANG_COMPILER) && MSVC_COMPLIER */
-#       define RuntimeAssert(c) ((U0)(c))
-#  endif       /*   GNU_COMPILER || CLANG_COMPILER && MSVC_COMPLIER  */
+#  else        /* !(COMPILER_HAS_BUILTINS) vs HAS_MSVC_INTRINSICS */
+#       define RuntimeAssert(c) ((u0)(c))
+#  endif       /*  COMPILER_HAS_BUILTINS   vs HAS_MSVC_INTRINSICS */
 #else          /* !RUNTIME_ASSERT */
-#       define RuntimeAssert(c) ((U0)(c))
+#       define RuntimeAssert(c) ((u0)(c))
 #endif         /* RuntimeAssert   */
 
-/* HintAssert: Enable UndefinedBehaviourSanitizer by passing -fsanitize=undefined.       */
-/* If compiled on higher optimization flags without -fsanitize, __builtin_unreachable()  */
-/* (GCC/Clang) or __assume(0) (MSVC) optimizes out the invalidated path; the compiler    */
-/* skips generating assembly for the while loop altogether regardless if the conditional */
-/* is true or false.                                                                     */
-
-/* Thus, prefer HintAssert as it also allows trapping of illegal instructions by passing */
-/* -fsanitize-trap=undefined (if needed) for debug builds. For release builds, simply    */
-/* build without passing -fsanitize.                                                     */
-
-/* [ Clang ] https: //clang.llvm.org/docs/UndefinedBehaviorSanitizer.html#ubsan-checks   */
-/* [ GCC   ] https: //gcc.gnu.org/onlinedocs/gcc/Instrumentation-Options.html            */
-/* [ MSVC  ] https: //learn.microsoft.com/en-us/cpp/intrinsics/assume?view=msvc-170      */
+/* HintAssert: Enable UndefinedBehaviourSanitizer by passing -fsanitize=undefined.    
+   If compiled on higher optimization flags without -fsanitize, __builtin_unreachable()  
+   (GCC/Clang) or __assume(0) (MSVC) optimizes out the invalidated path; the compiler    
+   skips generating assembly for the while loop altogether regardless if the conditional 
+   is true or false.                                                                     
+   
+   Thus, prefer HintAssert as it also allows trapping of illegal instructions by passing 
+   -fsanitize-trap=undefined (if needed) for debug builds. For release builds, simply    
+   build without passing -fsanitize.                                                     
+   
+   [ Clang ] https: //clang.llvm.org/docs/UndefinedBehaviorSanitizer.html#ubsan-checks   
+   [ GCC   ] https: //gcc.gnu.org/onlinedocs/gcc/Instrumentation-Options.html            
+   [ MSVC  ] https: //learn.microsoft.com/en-us/cpp/intrinsics/assume?view=msvc-170    */
 
 #if HINT_ASSERT
-#  if GNU_COMPILER || CLANG_COMPILER
+#  if COMPILER_HAS_BUILTINS
 #       define HintAssert(c) while (Unlikely(!(c))) __builtin_unreachable()
-#  elif MSVC_COMPILER
+#  elif HAS_MSVC_INTRINSICS
 #       define HintAssert(c) while (Unlikely(!(c))) __assume(0)
-#  else        /* !(GNU_COMPILER || CLANG_COMPILER || MSVC_COMPLIER) */
-#       define HintAssert(c) ((U0)(c))
-#  endif       /*  GNU_COMPILER || CLANG_COMPILER */
+#  else        /* !(COMPILER_HAS_BUILTINS || HAS_MSVC_INTRINSICS) */
+#       define HintAssert(c) ((u0)(c))
+#  endif       /*  COMPILER_HAS_BUILTINS */
 #else          /* !HINT_ASSERT */
-#       define HintAssert(c) ((U0)(c))
+#       define HintAssert(c) ((u0)(c))
 #endif         /* HintAssert */
 
 #if RUNTIME_ASSERT && HINT_ASSERT
@@ -101,7 +98,7 @@
 #       define Assert(c) RuntimeAssert(c)
 #  endif       /*  ASSERT_USE_HINT */
 #else          /* !(RUNTIME_ASSERT && HINT_ASSERT) */
-#       define Assert(c) ((U0)(c))
+#       define Assert(c) ((u0)(c))
 #endif         /* Assert */
 
 /**........................................................
@@ -116,11 +113,11 @@
 #       define DebugBreak() asm("int3")
 #  elif ARCH_ARM64
 #       define DebugBreak() asm("brk\t#0x666")
-#  elif GNU_COMPILER || CLANG_COMPILER
+#  elif COMPILER_HAS_BUILTINS
 #       define DebugBreak() __builtin_trap()
-#  endif       /* ARCH_X64 || ARCH_ARM64 || GNU_COMPILER || CLANG_COMPILER */
+#  endif       /* ARCH_X64 || ARCH_ARM64 vs COMPILER_HAS_BUILTINS */
 #else          /* !DEBUG */
-#       define DebugBreak() ((U0)0)
+#       define DebugBreak() ((u0)0)
 #endif         /* DebugBreak() */
 
 /**........................................................
@@ -137,11 +134,13 @@
 #endif         /* ASAN_ENABLED */
 
 #if DEBUG && ASAN_ENABLED
+C_LINKAGE void __asan_poison_memory_region(void const volatile *addr, size_t size);
+C_LINKAGE void __asan_unpoison_memory_region(void const volatile *addr, size_t size);
 #       define AsanPoisonMemRegion(a, s)   __asan_poison_memory_region((a), (s))
 #       define AsanUnpoisonMemRegion(a, s) __asan_unpoison_memory_region((a), (s))
 #else          /* !(DEBUG && ASAN_ENABLED) */
-#       define AsanPoisonMemRegion(a, s)   ((U0)(a), (U0)(s))
-#       define AsanUnpoisonMemRegion(a, s) ((U0)(a), (U0)(s))
+#       define AsanPoisonMemRegion(a, s)   ((u0)(a), (u0)(s))
+#       define AsanUnpoisonMemRegion(a, s) ((u0)(a), (u0)(s))
 #endif         /* DBG_PoisonMemRegion && DBG_UnpoisonMemRegion */
 
 /**........................................................
@@ -161,18 +160,18 @@
 // Poisoned pointers and unmmappable memory markers      */
 
 /* TODO: HandleInvalidate */
-#if GNU_COMPILER || CLANG_COMPILER
-#       define PtrInvalidate(ptr, n) ({                                                          \
-               CompileAssert(__builtin_classify_type(ptr) == __builtin_classify_type((U0*)0) &&  \
-                             __builtin_classify_type(n)   == __builtin_classify_type((I32)0),    \
+#if COMPILER_HAS_BUILTINS
+#       define PtrInvalidate(ptr, n)                                                             \
+        ({     CompileAssert(__builtin_classify_type(ptr) == __builtin_classify_type((u0*)0) &&  \
+                             __builtin_classify_type(n)   == __builtin_classify_type((i32)0),    \
                              "PtrInvalidate: a must be a ptr and n must be an int/hex value.");  \
-               do { (ptr) = ((U0*)n); } while(0);                                                \
+               do { (ptr) = ((u0*)n); } while(0);                                                \
         })
-#else          /* !(GNU_COMPILER || CLANG_COMPILER) */
-#       define PtrInvalidate(ptr, n) do { (ptr) = ((U0*)n); } while(0)
+#else          /* !(COMPILER_HAS_BUILTINS) */
+#       define PtrInvalidate(ptr, n) do { (ptr) = ((u0*)n); } while(0)
 #endif         /* PtrInvalidate */
 
-#define PtrIsInvalidated(ptr, n) ((ptr) == (U0*)(Iptr)n)
+#define PtrIsInvalidated(ptr, n) ((ptr) == (u0*)(iptr)n)
 
 /* base_list.h && base_list.c */
 #define LISTNODE_POISON_PREVPTR 0xDEAD0001
